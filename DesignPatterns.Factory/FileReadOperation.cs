a39da0ef8247f77ffeeab9102ee19d7cdda05ff5
A000917542 <56136004+A000917542@users.noreply.github.com>
2024-03-27 01:27:57 +0000

    Factory

---
+++ DesignPatterns.Factory/FileReadOperation.cs
@@ -0,0 +1,12 @@
+namespace DesignPatterns.Factory;
+
+using System.IO;
+
+public class FileReadOperation : IFileOperation
+{
+    public void Execute(string filePath)
+    {
+        string content = File.ReadAllText(filePath);
+        Console.WriteLine(content);
+    }
+}
